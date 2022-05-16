5cdf2e572913fbed90b70f46adb81e347c57f4e5
rameshkorla <rameshkorla.udemy@gmail.com>
2022-05-16 09:53:00 +0530

    update the cucumber dependencies

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -2,8 +2,9 @@ package cucumberOptions;
 
 import org.junit.runner.RunWith;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
--- src/test/java/stepDefinations/stepDefination.java
+++ src/test/java/stepDefinations/stepDefination.java
@@ -4,12 +4,11 @@ package stepDefinations;
 
 import org.junit.runner.RunWith;
 
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-
-import cucumber.api.junit.Cucumber;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.junit.Cucumber;
 
 
 
